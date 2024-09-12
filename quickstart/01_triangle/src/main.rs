c0aa49e18c7e7764000f430f8724492625830cc4
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-09-12 11:53:08 +0100

    task 01 check passed

--- quickstart/01_triangle/src/main.rs
+++ quickstart/01_triangle/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut iter = stdin.lock().lines().take(3);
+
+    let a = iter.next().unwrap().unwrap().parse::<u32>().unwrap();
+    let b = iter.next().unwrap().unwrap().parse::<u32>().unwrap();
+    let c = iter.next().unwrap().unwrap().parse::<u32>().unwrap();
+
+    let result = if a + b > c && a + c > b && b + c > a {
+        "YES"
+    } else {
+        "NO"
+    };
+
+    println!("{}", result);
 }
