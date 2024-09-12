42f691ee3f35ca518da17e460103c26518c7ea8f
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-09-12 16:31:42 +0100

    task 05 solution with min_by_key

--- quickstart/05_nearest_number/src/main.rs
+++ quickstart/05_nearest_number/src/main.rs
@@ -23,28 +23,10 @@ fn main() {
         .parse::<isize>()
         .unwrap();
 
-    // numbers.sort();
+    let result = numbers
+        .iter()
+        .min_by_key(|&&num| (num - target_number).abs())
+        .unwrap();
 
-    let mut delta = std::isize::MAX;
-    let mut nearest_number = std::isize::MAX;
-
-    for number in &numbers {
-        let current_delta = (number - target_number).abs();
-        if current_delta < delta {
-            delta = current_delta;
-            nearest_number = *number;
-        }
-        // else {
-        //     break;
-        // }
-    }
-
-    println!("{}", nearest_number);
-
-    // let result = numbers
-    //     .iter()
-    //     .min_by_key(|&&num| (num - target_number).abs())
-    //     .unwrap();
-
-    // println!("{}", result);
+    println!("{}", result);
 }
