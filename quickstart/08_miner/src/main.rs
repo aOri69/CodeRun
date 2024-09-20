fa04d79e4c3d8ab1cf73d6af3010bccba2fb2a9d
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-09-20 16:54:33 +0100

    input parser into Field struct

--- quickstart/08_miner/src/main.rs
+++ quickstart/08_miner/src/main.rs
@@ -1,6 +1,45 @@
 use std::io::{self, BufRead};
 
-fn main() {
+#[derive(Debug)]
+struct MineCoord {
+    row: usize,
+    col: usize,
+}
+
+impl MineCoord {
+    fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Field {
+    rows: usize,
+    cols: usize,
+    mines: Vec<MineCoord>,
+}
+
+impl Field {
+    fn set_rows(&mut self, rows: usize) {
+        self.rows = rows;
+    }
+
+    fn set_cols(&mut self, cols: usize) {
+        self.cols = cols;
+    }
+
+    fn add_mine(&mut self, mine: MineCoord) {
+        self.mines.push(mine);
+    }
+}
+
+impl std::fmt::Display for Field {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+fn parse_input_field() -> Field {
     let mut stdin_iter = io::stdin().lock().lines();
 
     let first_line = stdin_iter
@@ -12,11 +51,27 @@ fn main() {
         .collect::<Result<Vec<_>, _>>()
         .unwrap();
 
-    let _rows = *first_line.first().unwrap();
-    let _cols = *first_line.get(1).unwrap();
-    let mines = *first_line.get(2).unwrap();
+    let mut field = Field::default();
 
-    while let Some(Ok(mine_coord)) = stdin_iter.take(mines).next() {
-        todo!();
+    field.set_rows(*first_line.first().unwrap());
+    field.set_cols(*first_line.get(1).unwrap());
+
+    let mines = *first_line.get(2).unwrap();
+    let mut mines_iter = stdin_iter.take(mines);
+
+    while let Some(Ok(mine_coord_str)) = mines_iter.next() {
+        let (row, col) = mine_coord_str.split_once(" ").unwrap();
+        field.add_mine(MineCoord::new(
+            row.parse::<usize>().unwrap(),
+            col.parse::<usize>().unwrap(),
+        ));
     }
+
+    field
+}
+
+fn main() {
+    let field = parse_input_field();
+
+    dbg!(field);
 }
